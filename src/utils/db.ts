295f67841f0af5b3a7fb4737f50efa02f7f304d6
Daniel Biedma <info@dbr.io>
2019-02-22 01:34:52 +0100

    now we store name and created at

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,8 +1,24 @@
-export const storeToken = (db, { teamId, token }) =>
-    db.collection('tokens').insertOne({ teamId, token });
+import { DateTime } from 'luxon';
+
+export const storeToken = (db, { teamId, token, teamName, deleted }) =>
+    db
+        .collection('tokens')
+        .findOneAndReplace(
+            { teamId },
+            {
+                teamId,
+                token,
+                teamName,
+                deleted: deleted ? deleted : false,
+                createdAt: DateTime.utc().toISO()
+            }
+        );
+
 export const getToken = (db, teamId) => db.collection('tokens').findOne({ teamId });
 export const storeMsgFile = (db, { teamId, channelId, fileId, ts }) =>
-    db.collection('event-files').insertOne({ teamId, channelId, fileId, ts });
+    db
+        .collection('event-files')
+        .insertOne({ teamId, channelId, fileId, ts, createdAt: DateTime.utc().toISO() });
 export const getMsgFile = (db, teamId, fileId) =>
     db.collection('event-files').findOne({ teamId, fileId });
 export const deleteMsgFile = (db, teamId, fileId) =>
