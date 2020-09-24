1a503e3a72e5192a589e9e7f5d187cfab62ffb36
Daniel Biedma <info@dbr.io>
2020-09-24 10:01:04 +0200

    missing action

--- src/logic/index.ts
+++ src/logic/index.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from 'events'
 import actionDeleteAll from './action.deleteAll'
+import actionDeleteImage from './action.deleteImage'
 import actionDeleteImageFromEvent from './action.deleteImageFromEvent'
 import actionDeleteImageFromMenu from './action.deleteImageFromMenu'
 import actionDeleteMessage from './action.deleteMessage'
@@ -15,6 +16,7 @@ asyncSlack.on('event::app_uninstalled', eventAppUnninstalled)
 asyncSlack.on('action::delete_all', actionDeleteAll)
 asyncSlack.on('action::delete_image_from_event', actionDeleteImageFromEvent)
 asyncSlack.on('action::delete_image_from_menu', actionDeleteImageFromMenu)
+asyncSlack.on('action::delete_image', actionDeleteImage)
 asyncSlack.on('action::delete_message', actionDeleteMessage)
 asyncSlack.on('slash::list', slashList)
 asyncSlack.on('slash::help', slashHelp)
